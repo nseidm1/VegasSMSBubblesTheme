5a45f7fae3c9ecf6b97bdad326b19d0368f72745
nseidm1 <noah@noahseidman.com>
2014-01-24 09:40:51 -0500

    Use selectors for backgrounds, and set a custom default wallpaper.

--- VegasSMSBubblesTheme/src/com/project/vegasbubbles/VegasSMSBubbleTheme.java
+++ VegasSMSBubblesTheme/src/com/project/vegasbubbles/VegasSMSBubbleTheme.java
@@ -6,16 +6,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-public class ThemePicker extends Activity {
+public class VegasSMSBubbleTheme extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		overridePendingTransition(0, 0);
 		try {
-			Intent intent = new Intent("com.project.vegassms.THEME_PICKER");
+			Intent intent = new Intent("com.project.vegassms.VEGAS_MESSAGER_THEME_PICKER");
 			startActivity(intent);
 		} catch(ActivityNotFoundException e) {
 			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=com.project.vegassms")));
 		}
 		finish();
+		overridePendingTransition(0, 0);
 	}
 }
